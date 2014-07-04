42c0a848d48e0cc4fe87ff340632f9cb0f9dd323
sweetymehta <ashish.saini@aequor.com>
2014-07-04 13:18:40 +0530

    intial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Railscast sm-rc133
+===================
+capistrano
+````
+It is ruby program that is set of tools to deploy your application to server
+```
+Gemfile
+```
+gem 'capistrano' , group: :development
+bundle install
+```
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmRc133::Application.config.secret_key_base = '0234b6c67603e6dfae7ea7eb93d5478b0c488301c8a34df95db90da37761c275067ad65663c0f236a519bba4d4f49ff0b036119348473e197e14d1cd43136e09'
